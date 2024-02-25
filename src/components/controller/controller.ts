5d5f2bf5b0200c0c4673bf9652b0f977b9ef8a13
Arkhipenka <europeoplebelarus@gmail.com>
2024-02-25 22:28:52 +0100

    feat: recoder to components.ts

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,22 +1,20 @@
 import AppLoader from './appLoader';
 
-interface AppLoaderRespConfig {
-  endpoint: string;
-  options?: Record<string, unknown>;
-}
-
-type CallbackFunction = (data: any) => void; 
 
 class AppController extends AppLoader {
   
-  getSources(callback: CallbackFunction): void {
-    const config: AppLoaderRespConfig = {
-      endpoint: 'sources',
-    };
-    super.getResp(config, callback);
+  getSources(callback) {
+    super.getResp(
+        {
+            endpoint: 'sources';
+        },
+        callback
+    )
+    
+    
   }
 
-  getNews(e: Event, callback: CallbackFunction): void {
+  getNews(e: Event, callback){
     let target = e.target as HTMLElement;
     const newsContainer = e.currentTarget as HTMLElement;
 
@@ -25,11 +23,15 @@ class AppController extends AppLoader {
         const sourceId = target.getAttribute('data-source-id');
         if (sourceId && newsContainer.getAttribute('data-source') !== sourceId) {
           newsContainer.setAttribute('data-source', sourceId);
-          const config: AppLoaderRespConfig = {
-            endpoint: 'everything',
-            options: { sources: sourceId },
-          };
-          super.getResp(config, callback);
+          super.getResp(
+            {
+                endpoint: 'everything',
+                options: {
+                    sources: sourceId,
+                },
+            },
+            callback
+          );
         }
         return;
       }
